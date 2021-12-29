1a34081d3de2be3c1324ebc62aa4cdf4de16805d
Okimii <92546867+Okimii@users.noreply.github.com>
2021-12-28 23:58:19 -0800

    Update example.py

--- example.py
+++ example.py
@@ -1,9 +1,12 @@
 #example
+import discordheximals
+
+
 @bot.command()
 async def command(ctx):
     embed = discord.Embed(
         title="title",
         description="description",
-        color=heximals.Color.air_force_blue(),
+        color=discordheximals.Color.air_force_blue(),
         timestamp=datetime.datetime.utcnow())
     await ctx.send(embed=embed)
