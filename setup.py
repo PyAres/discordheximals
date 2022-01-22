db03f2b7355504d3200575fdbb07584bfb771aee
Okimii <92546867+Okimii@users.noreply.github.com>
2022-01-21 23:23:11 -0400

    added setup file <3

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="discordheximals",
+    version="0.0.3",
+    author="okimii",
+    description="returns hexcode by name for discord.py and its forks",
+    url="https://github.com/Okimii/discordheximals",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",)
